9fd49993b6d70e03bf761b3047e88c72191f83ea
Peter Besze <pbesze@gmail.com>
2017-10-18 20:44:37 +0200

    Update Application.java

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -25,7 +25,13 @@ public class Application {
         staticFiles.location("/public");
         staticFiles.expireTime(600L);
         enableDebugScreen();
-
+        static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+        }
         // Set up before-filters (called before each get/post)
         before("*",                  Filters.addTrailingSlashes);
         before("*",                  Filters.handleLocaleChange);
